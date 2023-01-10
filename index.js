92e5a8172f5636b16bcc8a210434c2a96caf4ee9
Al Mamun Khan <almamunkhan09@gmail.com>
2023-01-10 15:49:03 +0100

    All the features added to the project

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-import chalk from 'chalk';
 // import packages
+import chalk from 'chalk';
 import randomcolor from 'randomcolor';
 
-const randomColor = randomcolor({ hue: 'blue', luminosity: 'light' });
-console.log(randomColor);
+// Initializing error messageand error color by chalk js
 
-const consoleColor = chalk.hex(randomColor);
+const hueError = 'input Color is not supported';
+const luminosityError = 'Input luminosity is not supported';
+const error = chalk.bold.red;
 
-const outputString = `###############################
+// Defining valid color
+
+const validHue = ['', 'blue', 'red', 'green'];
+const validLuminosity = ['', 'bright', 'dark', 'light'];
+
+// define generate color function
+const generateRandomColor = (inputHeu, inputLuminosity) => {
+  return randomcolor({ hue: inputHeu, luminosity: inputLuminosity });
+};
+
+// checking if the user input is valid
+// valid colors are blue,green,red
+// valid luminosities are bright , light,dark
+
+const inputHue = !process.argv[2] ? '' : process.argv[2].toLowerCase();
+const inputLuminosity = !process.argv[3] ? '' : process.argv[3].toLowerCase();
+
+if (!validHue.includes(inputHue)) {
+  console.log(error(hueError)); // provide error message if the color is not valid
+} else if (!validLuminosity.includes(inputLuminosity)) {
+  console.log(error(luminosityError)); //provide error message if the luminosity is not valid
+} else {
+  const randomColor = generateRandomColor(inputHue, inputLuminosity);
+  // console.log(randomColor); //// to check if the generateRandomColor is working
+
+  // Initialze the output that should look like
+
+  const outputString = `###############################
 ###############################
 ###############################
 #####                     #####
@@ -17,4 +45,6 @@ const outputString = `###############################
 ###############################
 ###############################`;
 
-console.log(consoleColor(outputString));
+  const consoleColor = chalk.hex(randomColor); // defines the color to output
+  console.log(consoleColor(outputString));
+}
